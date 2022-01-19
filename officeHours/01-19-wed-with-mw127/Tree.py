3cf9059705441cf9cf7faf9bc4aa622e84502af8
c212 <40395648+c212@users.noreply.github.com>
2022-01-19 12:30:01 -0500

    providing a remove

--- officeHours/01-19-wed-with-mw127/Tree.py
+++ officeHours/01-19-wed-with-mw127/Tree.py
@@ -18,12 +18,26 @@ class Tree:
             right = self.right.show()
         # return "(" + left + " " + str(self.value) + " " + right + ")"
         return "(" + str(self.value) + " " + left + " " + right + ")"
+    def remove(self, value):
+        if self.value == value:
+            if self.left == None:
+                return self.right
+            else:
+                a = largestIn(self.left)
+                return Tree(a, self.left.remove(a), self.right)
+        elif self.value > value: # I assume the value can be found in 
+            return Tree(self.value, self.left.remove(value), self.right)
+        else: # if the value does not exist the code needs to protect itself against that 
+            return Tree(self.value, self.left, self.right.remove(value))
+
 
 def main():
     a = Tree(3, None, None)
-    print(largestIn(a)) # 3
-    a .right = Tree(5, None, None)
-    print(largestIn(a)) # 5
+    a.right = Tree(5, None, None)
+    a.right.left = Tree(4, None, None)
+    print(a.show()) # (3 . (5 (4 . .) .))
+    b = a.remove(5)
+    print("Remove 5 the tree becomes: " + b.show())
 
 if __name__=="__main__":
     main()
