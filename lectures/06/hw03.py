b17993da1ca34aaadbd2a86b481f2ef439834b39
c212 <40395648+c212@users.noreply.github.com>
2022-01-31 11:50:15 -0500

    how to start hw03

---
+++ lectures/06/hw03.py
@@ -0,0 +1,7 @@
+assert(round(2.1) == 2), "Test 01 failed."
+assert(int(-2.1) == -2), "Test 02 failed."
+assert(round(-2.9) == -3), "Test 03 failed."
+assert(round(2.9) == 3), "Test 04 failed."
+assert(round(2.1) == 2), "Test 05 failed."
+assert(round(2.9) == 3), "Test 06 failed."
+print("All tests passed.")
